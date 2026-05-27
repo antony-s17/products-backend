62553f1d7e997a2515a8c0609cf1bb351f78bac0
Antony Salas <salasra1795@gmail.com>
2026-05-26 23:45:13 -0500

    feat: add endpoints to list all products and get product by id

--- src/app.js
+++ src/app.js
@@ -1,9 +1,39 @@
 import express from 'express';
+import productRouter from './routes/products.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
+
+
+//Routes
+app.get('/health', (req, res) => {
+    res.status(200).json(
+        {
+            ok: true,
+            data: "Server is up and running"
+        }
+    );
+});
+
+app.use('/api/products', productRouter);
+
+
+
+// Not found route
+app.use((req, res) => {
+    res.status(404).json(
+        {
+            ok: false,
+            error: {
+                code: 'ROUTE_NOT_FOUND',
+                message: 'Not found'
+            }
+        }
+    )
+})
+
 
 export default app;
