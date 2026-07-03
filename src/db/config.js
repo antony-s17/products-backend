bd7d6990550e4c28c42cda370e8fdf8af3c74d8d
Antony Salas <salasra1795@gmail.com>
2026-07-02 20:22:36 -0500

    feat: add connection to mongodb

--- src/db/config.js
+++ src/db/config.js
@@ -1,8 +1,14 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { DATABASE_URL } from "../misc/constants.js";
+import { DATABASE_URL, MONGO_DB_URI } from "../misc/constants.js";
+import mongoose from "mongoose";
 
 const adapter = new PrismaPg({ connectionString: DATABASE_URL });
 const prisma = new PrismaClient( { adapter });
 
-export default prisma;
+const mongoDB = async() => {
+    await mongoose.connect(MONGO_DB_URI);
+    console.log("Connected to MongoDB");
+}
+
+export { prisma, mongoDB };
