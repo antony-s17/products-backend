d531cf18141d88c77d431bf595a41f13e928471c
Antony Salas <salasra1795@gmail.com>
2026-07-04 16:23:26 -0500

    feat: add mongodb url constant

--- src/misc/constants.js
+++ src/misc/constants.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL;
 const JWT_SECRET = process.env.JWT_SECRET;
 const MONGO_DB_URI = process.env.MONGO_DB_URI;
